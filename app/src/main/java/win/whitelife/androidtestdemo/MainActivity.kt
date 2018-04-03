0a08469d7d0a2051250817bbf212cefe067bd433
whitelife <976397068@qq.com>
2018-04-03 17:20:25 +0800

    Update MainActivity.kt
    u

--- app/src/main/java/win/whitelife/androidtestdemo/MainActivity.kt
+++ app/src/main/java/win/whitelife/androidtestdemo/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         var m=ConcurrentHashMap<String,String>()
         m.put("","")
         bt_click2.setOnClickListener({
-            tv_main.setText("点击了按键2")
+            tv_main.setText("点击了按键1")
         })
          bt_click2.setOnClickListener({
             tv_main.setText("点击了按键2")
