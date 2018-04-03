b0769399c9f88d60ba9b3e30c6a0df8efc9960f2
wuzefeng <976397068@qq.com>
2018-04-03 17:18:16 +0800

    update

--- app/src/main/java/win/whitelife/androidtestdemo/MainActivity.kt
+++ app/src/main/java/win/whitelife/androidtestdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*;
+import java.util.concurrent.ConcurrentHashMap
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         bt_click.setOnClickListener({
             Toast.makeText(this,"点击了按钮",Toast.LENGTH_SHORT).show()
         })
+        var m=ConcurrentHashMap<String,String>()
+        m.put("","")
         bt_click2.setOnClickListener({
             tv_main.setText("点击了按键2")
         })
